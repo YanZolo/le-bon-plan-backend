3fa15a550227aeb163a043b5d558e20213c47bc1
sofianeSghiouar <sofianesgh@gmail.com>
2022-02-23 11:29:47 +0100

    update: merging branch test/product: some refactoring performed

--- src/controllers/users/usersController.js
+++ src/controllers/users/usersController.js
@@ -5,46 +5,37 @@ class UserController {
     getAllUsers() {
         return this.#users;
     };
-
     getUser({_id}) {
         const user = this.#users.filter(user => user._id === _id)[0];
         return user;
-    }
-
+    };
    saveUser(user) {
        const newUser = {
            _id: user._id || v4(),
            name: user.name
        };
-       this.#users.push(newUser)
-       return newUser
-   }
-
-
+       this.#users.push(newUser);
+       return newUser;
+   };
    updateUser({_id, name}) {
     const user = this.#users.filter(user => user._id === _id)[0];
     if(user){
         const indexUser = this.#users.indexOf(user);    
         this.#users[indexUser].name = name;
         return user;
-    }
-     throw new Error('User Not Found !')
-   }
-
+    };
+     throw new Error('User Not Found !');
+   };
    deleteUser({_id}) {
        const user = this.#users.filter(user => user._id === _id)[0];
        if(user) {
            const userIndex = this.#users.indexOf(user);
            delete this.#users[userIndex];
            return user;
-       }
-       throw new Error('User Not Found !')
-   }
-
-
-
-}
-
+       };
+       throw new Error('User Not Found !');
+   };
+};
 module.exports = {
     UserController
-}
+};
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const userRoutes = require('./controllers/users/routes');
-const productRoutes = require('./controllers/products/routes')
-const startDB = require('./db/connect')
-const path = require('path')
+const productRoutes = require('./controllers/products/routes');
+const startDB = require('./db/connect');
+const path = require('path');
 const url = process.env.DB_URL;
 
 
@@ -14,19 +14,19 @@ app.use(express.urlencoded({extended: true}));
 
 
 app.disable('x-powered-by'); //Disable this header, to prevent attacks. (use helmet will be a best protection)
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, '..' , 'views'))
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '..' , 'views'));
 
 
 
 
 app.get('/', (req, res) => {
-    res.render('login')
+    res.render('login');
 })
 app.use('/user', userRoutes);
 app.use('/product', productRoutes);
 app.get('/health', (req, res) => {
-    res.send('ok')
+    res.send('ok');
 })
 
 const connect = async () => {
@@ -36,5 +36,5 @@ const connect = async () => {
     });
 }
 
-connect()
+connect();
 
